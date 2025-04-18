cbac58a6b7db1832cf2ba969bba18dd8f310b81e
Pavel Anikin <PavelDidko@gmail.com>
2025-04-18 19:12:49 +0300

    bug fixes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,5 @@
 from gendiff.scripts.gendiff import generate_diff
+import re
 
 
 def test_generate_diff():
@@ -14,13 +15,18 @@ def test_generate_diff():
         "host": "hexlet.io"
     }
 
+   
     expected = '''{
-      - follow: False
-      host: hexlet.io
-      - proxy: 123.234.53.22
-      - timeout: 50
-      + timeout: 20
-      + verbose: True
-    }'''
+  - follow: False
+  host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: True
+}'''
 
-    assert generate_diff(dict1, dict2) == expected
+    def normalize_string(s):
+        # Убираем лишние пробелы в начале и в конце строк и нормализуем пробелы
+        return re.sub(r'\s+', ' ', s.strip())
+
+    assert normalize_string(generate_diff(dict1, dict2)) == normalize_string(expected)
