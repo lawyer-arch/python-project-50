c666deade3f24f34497c6c2c52234a903ad3a55c
Pavel Anikin <PavelDidko@gmail.com>
2025-04-18 18:24:23 +0300

    add testes and test

---
+++ tests/test_gendiff.py
@@ -0,0 +1,23 @@
+def test_generate_diff():
+    dict1 = {
+        "host": "hexlet.io",
+        "timeout": 50,
+        "proxy": "123.234.53.22",
+        "follow": False
+    }
+    dict2 = {
+        "timeout": 20,
+        "verbose": True,
+        "host": "hexlet.io"
+    }
+
+    expected = '''{
+      - follow: False
+      host: hexlet.io
+      - proxy: 123.234.53.22
+      - timeout: 50
+      + timeout: 20
+      + verbose: True
+    }'''
+
+    assert generate_diff(dict1, dict2) == expected
